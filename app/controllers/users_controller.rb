77518d2d9e075299afcbb099fa59b2ee65568e7b
EC2 Default User <ec2-user@ip-172-31-6-170.us-east-2.compute.internal>
2018-05-11 12:11:58 +0000

    first comment

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,12 @@
+class UsersController < ApplicationController
+    def index
+    end
+
+  class UsersController < ApplicationController
+    def index
+    end
+
+    def show
+        @user = User.find_by(:username => params[:username])
+    end
+end
